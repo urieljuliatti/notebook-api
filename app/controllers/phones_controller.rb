618af1e4dc464c38ea5bf0e39767abdcbf841d42
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-28 12:27:55 -0300

    Crud do PhonesController

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,12 +1,40 @@
 class PhonesController < ApplicationController
-  before_action :set_phone
+  before_action :set_contact
+
+  def create
+    @contact.phones << Phone.new(phone_params)
+    if @contact.save
+      render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    phone = Phone.find(phone_params[:id])
+    if phone.update(phone_params)
+      render json: @contact.phones
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
 
   def show
-    render json: @phones
+    render json: @contact.phones
+  end
+
+  def destroy
+    phone = Phone.find(phone_params[:id])
+    phone.destroy
   end
 
   private
-  def set_phone
-    @phones = Contact.find(params[:contact_id]).phones
+
+  def set_contact
+    @contact = Contact.find(params[:contact_id])
+  end
+
+  def phone_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
   end
 end
