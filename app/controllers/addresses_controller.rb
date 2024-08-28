898f0a61767f586f361d023adf557c66365054bd
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-28 11:13:15 -0300

    Finalizando o crud aninhado de AddressesController

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,12 +1,35 @@
 class AddressesController < ApplicationController
-  before_action :set_address
+  before_action :set_contact
 
-  def show
-    render json: @address
+  def create
+    @contact.address = Address.new(address_params)
+    if @contact.save
+      render json: @contact.address, status: :created, location: contact_address_url(@contact)
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
   end
 
+  def update
+    if @contact.address.update(address_params)
+      render json: @contact.address
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @contact.address
+  end
+
+  def destroy
+    @contact.address.destroy
+  end
   private
-  def set_address
-    @address = Contact.find(params[:contact_id]).address
+  def set_contact
+    @contact = Contact.find(params[:contact_id])
+  end
+  def address_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
   end
 end
